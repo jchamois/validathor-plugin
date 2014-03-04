985b3f1b6bebfd356707635eec5c22c846fdb106
jchamois <jeremy.chamois@gmail.com>
2014-03-04 18:11:21 +0100

    ajout title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Project STARTER</title>
+        <title>VALIDATHOR</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
            
@@ -25,7 +25,7 @@
 
 <body>
     <div class="size1of2 center">
-        <h1 class="h1">VALID FORMULAIRE</h1>
+        <h1 class="h1">VALIDATHOR</h1>
 
         <form action="#" method="post" class="form ">   
             <div class="row ">
